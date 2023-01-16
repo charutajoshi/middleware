ac3402e674d8203963b11216128b4b9f0e1ffaaf
charutaj <charutajoshi@Charutas-MacBook-Air.local>
2023-01-15 20:39:35 -0600

    Initial commit

---
+++ MiddlewareExample/Program.cs
@@ -0,0 +1,18 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+// Middleware 1
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("Hello\n");
+    await next(context); 
+});
+
+// Middleware 2
+app.Run(async (HttpContext context) =>
+{
+    await context.Response.WriteAsync("World");
+});
+
+app.Run();
+
